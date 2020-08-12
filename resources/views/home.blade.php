b5e567b7d35dde4b1cabdd1d62d448fa2bfebbad
Kulmambetov Atay <a.kulmambetov@yandex.ru>
2020-08-12 22:53:36 +0600

    fix render companies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,24 +4,22 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
-
                 @foreach($news as $new)
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title"><b>{{ $new->title }}</b></h5>
-                            <p class="card-text text-muted">{{ $new->description }}</p>
+                    @if($new->companies->where('status', 1)->isNotEmpty())
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title"><b>{{ $new->title }}</b></h5>
+                                <p class="card-text text-muted">{{ $new->description }}</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                @foreach($new->companies->where('status', 1) as $company)
+                                    <li class="list-group-item">{{ $company->title }}</li>
+                                @endforeach
+                            </ul>
                         </div>
-                        <ul class="list-group list-group-flush">
-                            @foreach($new->companies->where('status', 1) as $company)
-                                <li class="list-group-item">{{ $company->title }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-
-                    <hr>
-
+                        <hr>
+                    @endif
                 @endforeach
-
             </div>
         </div>
     </div>
